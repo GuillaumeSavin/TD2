da9a24919c9025fc5f2c2510bb3ff1f079328391
GuillaumeSavin <guisav1996@hotmail.fr>
2020-09-17 16:34:11 +0200

    exo3 commit

---
+++ Exo2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exo2
+{
+    class Article
+    {
+        public int prix = 0;
+
+        public int Prix
+        {
+            get => prix;
+            set => prix = value;
+        }
+
+
+        public void affichePrix()
+        {
+            Console.WriteLine("Prix: {0} euros", Prix);
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Article A = new Article();
+            A.Prix = 10;
+            A.affichePrix();
+        }
+    }
+}
---
+++ Exo3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace Exo3
+{
+    public class Personnes
+    {
+        private String[] m_Noms;
+        private int m_NbElt;
+        private int m_Max;
+
+        public Personnes(int Max)
+        {
+            this.m_Max = Max;
+            this.m_NbElt = 1;
+            this.m_Noms = new string[Max];
+        }
+
+        public int this[String Nom]
+        {
+            get
+            {
+                for (int i = 0; i < this.m_Noms.Length; i ++)
+                {
+                    if (Nom == this.m_Noms[i])
+                    {
+                        return i;
+                    }
+
+                    
+                }
+                return -1;
+            }
+        }
+
+        public String this[int i]
+        {
+            get
+            {
+                if (i > this.m_NbElt)
+                {
+                    return null;
+                }
+                return this.m_Noms[i];
+            }
+            set
+            {
+                if (i <= this.m_NbElt)
+                {
+                    this.m_Noms[i] = value;
+                }
+                
+                if (i == (this.m_NbElt + 1) && (i <= this.m_Max))
+                {
+                    this.m_NbElt++;
+                    this.m_Noms[i] = value;
+
+                }
+
+
+            }
+        }
+    }
+
+    class Prog
+    {
+        static void Main(String[] args)
+        {
+            Personnes Tableau = new Personnes(4);
+            Tableau[0] = "Anna";
+            Tableau[1] = "Ingrid";
+            Tableau[2] = "Maria";
+            Tableau[3] = "Ulrika";
+            Console.WriteLine(Tableau[1]);
+            Console.WriteLine(Tableau["Maria"]);
+            Console.WriteLine(Tableau[10]);
+            Console.WriteLine(Tableau["Toto"]);
+        }
+    }
+}
